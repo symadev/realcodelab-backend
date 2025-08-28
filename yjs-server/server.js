8a3558108cf6b84d3f4b99ea927239a4dacf8b8d
syma sultana <symasultana2002@gmail.com>
2025-08-28 18:02:01 +0600

    added yjs folder with make some changes

---
+++ yjs-server/server.js
@@ -0,0 +1,19 @@
+import { WebSocketServer } from 'ws'
+import { setupWSConnection } from 'y-websocket/bin/utils.js'
+import * as http from 'http'
+
+const port = 6000
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Yjs WebSocket server\n')
+})
+
+const wss = new WebSocketServer({ server })
+
+wss.on('connection', (conn, req) => {
+  setupWSConnection(conn, req)
+})
+
+server.listen(port, () => {
+  console.log(`Yjs WebSocket server running on port ${port}`)
+})
