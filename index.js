b7d700384c4f6fc28b479904f83d71d67ec9c57a
syma sultana <symasultana2002@gmail.com>
2025-08-30 21:56:00 +0600

    added commit

--- index.js
+++ index.js
@@ -8,28 +8,24 @@ import jwt from 'jsonwebtoken';
 import { connectMongo, getUserCollection, saveSnapshot } from './mongo.js';
 import { compileCode, getSubmission } from "./judge0.js";
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://rhtradingglobal.com"
-];
+const cors = require('cors');
 
-app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin like mobile apps or curl
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"],
-  credentials: true
-}));
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://rhtradingglobal.com',
+   
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+};
 
-// Make sure preflight requests are handled
-app.options("*", cors());
+app.use(cors(corsOptions));
+
+
+app.options('*', cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
 const app = express();
