f1ebd5fa03a8025c9ed4672bfce4f72500e912f3
syma sultana <symasultana2002@gmail.com>
2025-08-30 19:32:59 +0600

    added changes

--- index.js
+++ index.js
@@ -8,15 +8,28 @@ import jwt from 'jsonwebtoken';
 import { connectMongo, getUserCollection, saveSnapshot } from './mongo.js';
 import { compileCode, getSubmission } from "./judge0.js";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://rhtradingglobal.com"
+];
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://rhtradingglobal.com'],
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
+  origin: function(origin, callback) {
+    // allow requests with no origin like mobile apps or curl
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
   credentials: true
 }));
 
-// Optional: OPTIONS preflight response
-app.options('*', cors());
+// Make sure preflight requests are handled
+app.options("*", cors());
 
 const PORT = process.env.PORT || 5000;
 const app = express();
